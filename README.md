6712065f1437ae398b83d70aada34efcdfd561db
Francois Billant <fbillant@gmail.com>
2015-08-01 14:48:47 +0200

    Added an environment variable timezone to be used to set the TZ dynamically at instanciation

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## RUN
 The /var/log directory of the host is mount in the /var/log/host directory of the container, keep that in mind for you jail.local file (an example file can be found on the github repo)
 ```
-docker run -ti --rm --net=host --privileged -v /var/log:/var/log/host -v ~/fail2ban/jail.local:/etc/fail2ban/jail.local francois/fail2ban:latest
+docker run -ti --rm --net=host --privileged -e TIMEZONE="Europe/Paris" -v /var/log:/var/log/host -v ~/fail2ban/jail.local:/etc/fail2ban/jail.local francois/fail2ban:latest
 ```
 
 ## BUILD
--- run.sh
+++ run.sh
@@ -6,4 +6,19 @@
 # The first post is still relevant.
 # With fail2ban evolution, we should be able to to replace this scrip with a CMD or ENTRYPOINT in the Dockerfile
 
-/usr/local/bin/fail2ban-client -f start && tailf /var/log/fail2ban.log
+function check_timezone {
+    CURRENT_TZ=`cat /etc/timezone`
+    if [ -z $TIMEZONE ]; then
+        echo "The environment variable 'TIMEZONE' is not set, using default: $CURRENT_TZ";
+    else
+        if [ $CURRENT_TZ != $TIMEZONE ]; then
+            echo "Configuring timezone to \"$TIMEZONE\""
+            echo $TIMEZONE > /etc/timezone
+            dpkg-reconfigure -f noninteractive tzdata
+        else
+            echo "Timezone already configured"
+        fi
+    fi
+}
+
+check_timezone && /usr/local/bin/fail2ban-client -f start && tailf /var/log/fail2ban.log
